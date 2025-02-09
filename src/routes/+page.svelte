200fb4937452efa817995534ce222e200354e833
Tommy Rennolds <thomasrennolds@gmail.com>
2025-02-09 16:19:19 -0500

    new initial page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let count = $state(0);
+  </script>
+  
+  <div class="min-h-screen flex items-center justify-center flex-col gap-8 bg-gray-900 text-white">
+    <button 
+      onclick={() => count++}
+      class="text-4xl font-bold hover:text-blue-400 transition-colors duration-300"
+    >
+      CROSSFADE, CLICK HERE
+    </button>
+    
+    <div class="text-6xl font-mono">
+      {count}
+    </div>
+</div>
+</script>
