efe991a4c75bc04c0c86cd20a5b9dda9dc8b9888
Tommy Rennolds <thomasrennolds@gmail.com>
2025-02-15 15:16:49 -0500

    ad space added

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -39,9 +39,9 @@
     });
 </script>
   <div class="h-[50px] w-full bg-gray-200 md:hidden fixed top-0 left-0 right-0 z-50 flex items-center justify-center">
-    <span class="text-gray-500">Ad Space (320x50)</span>
+    <span class="text-gray-500"><p>Ad Space (320x50)</p></span>
   </div>
-  <nav class="bg-white border-b border-gray-200 fixed top-0 left-0 right-0 z-50">
+  <nav class="bg-white border-b border-gray-200 fixed md:top-0 left-0 right-0 z-50 top-[50px]">
     <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex justify-between h-14">
         <!-- Left side -->
