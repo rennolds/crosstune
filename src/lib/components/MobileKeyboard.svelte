8f9b06d302ea855c79cb5b1148a2e951c1b1b532
Tommy Rennolds <thomasrennolds@gmail.com>
2025-03-23 22:09:56 -0400

    reset

--- src/lib/components/MobileKeyboard.svelte
+++ src/lib/components/MobileKeyboard.svelte
@@ -11,9 +11,9 @@
   const symbolRow3 = ['.','!','?','/','(',')',';',':'];  // Added period here too
 </script>
 
-<div class="virtual-keyboard md:hidden fixed bottom-0 left-0 right-0 bg-gray-100 dark:bg-gray-900 shadow-lg pb-2 dark:text-black">
+<div class="virtual-keyboard md:hidden fixed bottom-0 left-0 right-0 light:bg-gray-100  bg-gray-100 shadow-lg pb-2 dark:text-black">
   <!-- Increased bottom padding -->
-  <div class="px-1 pb-1 space-y-2"> <!-- Reduced horizontal padding, increased vertical gap -->
+  <div class="px-1 pb-1 pt-1 space-y-2"> <!-- Reduced horizontal padding, increased vertical gap -->
     <!-- Row 1 -->
     <div class="flex gap-1">
       {#each (showSymbols ? symbolRow1 : letterRow1) as key}
