917f0d76c7dab7e9f1f62f8269a566d6db3e25c4
Tommy Rennolds <thomasrennolds@gmail.com>
2025-03-31 23:24:22 -0400

    ramp code included

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,47 @@
 <script>
-    import CrosswordGrid2 from '$lib/components/CrosswordGrid2.svelte';
-    import Navbar from '$lib/components/Navbar.svelte';
-    import SplashScreen from '$lib/components/SplashScreen.svelte';
-    
-    // Check if we should show splash on load (always show on main route)
-    
-    // References to hold the reveal functions
-    let revealSquare = $state(null);
-    let revealWord = $state(null);
-    let revealPuzzle = $state(null);
-    let showSplash = $state(true);
+  import CrosswordGrid2 from "$lib/components/CrosswordGrid2.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
+  import SplashScreen from "$lib/components/SplashScreen.svelte";
+  import Ramp from "$lib/components/Ramp.svelte";
 
-    function handlePlay() {
-        showSplash = false;
-    }
-    
-    // Function to receive the reveal functions from CrosswordGrid
-    function handleRevealFunctions(functions) {
-        revealSquare = functions.revealSquare;
-        revealWord = functions.revealWord;
-        revealPuzzle = functions.revealPuzzle;
-    }
-    
-    function navigateToHome() {
-        window.location.reload();
-    }
+  // Check if we should show splash on load (always show on main route)
+
+  const PUBLISHER_ID = 1025391;
+  const WEBSITE_ID = 75604;
+
+  // References to hold the reveal functions
+  let revealSquare = $state(null);
+  let revealWord = $state(null);
+  let revealPuzzle = $state(null);
+  let showSplash = $state(true);
+
+  function handlePlay() {
+    showSplash = false;
+  }
+
+  // Function to receive the reveal functions from CrosswordGrid
+  function handleRevealFunctions(functions) {
+    revealSquare = functions.revealSquare;
+    revealWord = functions.revealWord;
+    revealPuzzle = functions.revealPuzzle;
+  }
+
+  function navigateToHome() {
+    window.location.reload();
+  }
 </script>
 
-<Navbar 
-    onRevealSquare={revealSquare} 
-    onRevealWord={revealWord} 
-    onRevealPuzzle={revealPuzzle}
-    hideTimer={showSplash}
+<Navbar
+  onRevealSquare={revealSquare}
+  onRevealWord={revealWord}
+  onRevealPuzzle={revealPuzzle}
+  hideTimer={showSplash}
 />
 {#if showSplash}
-    
-    <SplashScreen onPlay={handlePlay} />
-
+  <SplashScreen onPlay={handlePlay} />
 {:else}
-    <main class="dark min-h-screen">
-        <CrosswordGrid2 
-            onSetRevealFunctions={handleRevealFunctions}
-        />
-    </main>
+  <main class="dark min-h-screen">
+    <Ramp PUB_ID={PUBLISHER_ID} {WEBSITE_ID} />
+    <CrosswordGrid2 onSetRevealFunctions={handleRevealFunctions} />
+  </main>
 {/if}
