4e050974a735565633a256e38651929e3e742f74
Tommy Rennolds <thomasrennolds@gmail.com>
2025-08-29 22:14:52 -0400

    server side auth callback working

---
+++ src/routes/auth/auth-code-error/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+<div>
+    <h1>Authentication Error</h1>
+    <p>There was an error authenticating your account. Please try again.</p>
+</div>
---
+++ src/routes/auth/callback/+server.js
@@ -0,0 +1,28 @@
+import { redirect } from '@sveltejs/kit';
+
+export const GET = async (event) => {
+	const {
+		url,
+		locals: { supabase }
+	} = event;
+
+	const code = url.searchParams.get('code');
+	const tokenHash = url.searchParams.get('token_hash');
+	const type = url.searchParams.get('type');
+	const next = url.searchParams.get('next') ?? '/';
+
+	let error = null;
+
+	if (code) {
+		({ error } = await supabase.auth.exchangeCodeForSession(code));
+	} else if (tokenHash && type) {
+		({ error } = await supabase.auth.verifyOtp({ token_hash: tokenHash, type }));
+	}
+
+	if (!error) {
+		throw redirect(303, `/${next.slice(1)}`);
+	}
+
+	console.error('Auth callback error:', error);
+	throw redirect(303, '/auth/auth-code-error');
+};
